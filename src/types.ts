7faa1f035bed324c3d396ee174f1d7de6040cf57
sgb-io <sam@sgb.io>
2024-03-10 13:45:28 +0000

    Update types

--- src/types.ts
+++ src/types.ts
@@ -6,4 +6,4 @@ export interface IncomeTax {
 
 export type StudentLoanPlan = 1 | 2 | 4 | 5 | "postgrad";
 
-export type TaxYear = "2022/23" | "2023/24";
+export type TaxYear = "2022/23" | "2023/24" | "2024/25";
